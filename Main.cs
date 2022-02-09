11e45a759276b10dc41d8baa71b9b2a562b9847f
Lucas <onitrog0d@gmail.com>
2022-02-09 03:36:42 -0300

    Initial commit v1.0.0 - 2.3

---
+++ Main.cs
@@ -0,0 +1,68 @@
+﻿using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using UnhollowerBaseLib;
+
+namespace ChecksumBypass {
+
+    public static class ModBuildInfo {
+        public const string Name = "GenshinChecksumBypass";
+        public const string Author = "nitro.";
+        public const string Version = "1.0.0";
+        public const string DownloadLink = null;
+        public const string Company = null;
+        public const string GameDeveloper = "miHoYo";
+        public const string Game = "Genshin Impact";
+    }
+
+    public class Main : MelonMod {
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate void PlayerLoginReqProtoStringSettersDelegate(IntPtr instance, IntPtr value, IntPtr nativeMethodPointer);
+
+        private static List<Delegate> dontGcDelegates = new List<Delegate>();
+
+        public override void OnApplicationStart() {
+            MelonLogger.Msg("Mod loaded.");
+            unsafe {
+                var properties = typeof(Object1PublicSealedIEquatable1ObfMJ1ObKJInOCStONKPInUnique).GetProperties(BindingFlags.Instance | BindingFlags.Public);
+
+                foreach (var property in properties.Where(p => (p.PropertyType == typeof(string)) && (p.SetMethod.CustomAttributes.Count() == 1))) {
+                    var originalMethod = property.SetMethod;
+
+                    PlayerLoginReqProtoStringSettersDelegate tempDelegate, originalDelegate = null;
+
+                    tempDelegate = (instance, value, nativeMethodPointer) => {
+                        var convertedString = IL2CPP.Il2CppStringToManaged(value);
+                        if (convertedString.Length == 66) {
+                            MelonLogger.Msg("Original checksum: " + convertedString);
+                            MelonLogger.Msg("Setting checksum in function " + originalMethod.Name);
+                            // var checksum2.2 = "601f9d24a9f983df28cdbfe40d6ca4d0c30f8297a7b37ed3b7a082df15aff9771d";
+                            // var checksum2.3 = "37dfc56fd2bf5fa0d3ec55b4f2a81d374c9c8c5382645c32f97495469dbf036e1f";
+                            var checksum = "c1218c188819cba2e794c29c6f5c0f2248eefb4cd29f29b18619e7626f83044a20";
+                            originalDelegate(instance, IL2CPP.ManagedStringToIl2Cpp(checksum), nativeMethodPointer);
+                        } else {
+                            originalDelegate(instance, value, nativeMethodPointer);
+                        }
+                    };
+
+                    dontGcDelegates.Add(tempDelegate);
+
+                    var originalMethodPointer = *(IntPtr*)(IntPtr)UnhollowerUtils.GetIl2CppMethodInfoPointerFieldForGeneratedMethod(originalMethod).GetValue(null);
+                    var delegateDetourPointer = Marshal.GetFunctionPointerForDelegate(tempDelegate);
+
+                    MelonUtils.NativeHookAttach((IntPtr)(&originalMethodPointer), delegateDetourPointer);
+
+                    originalDelegate = Marshal.GetDelegateForFunctionPointer<PlayerLoginReqProtoStringSettersDelegate>(originalMethodPointer);
+                }
+
+                MelonLogger.Msg("Hooked PlayerLoginReq proto setters.");
+            }
+            
+        }
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using ChecksumBypass;
+using MelonLoader;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle(ModBuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(ModBuildInfo.Company)]
+[assembly: AssemblyProduct(ModBuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + ModBuildInfo.Author)]
+[assembly: AssemblyTrademark(ModBuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9d02aa87-5222-4c13-9b79-daed77acf4c4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(ModBuildInfo.Version)]
+[assembly: AssemblyFileVersion(ModBuildInfo.Version)]
+[assembly: MelonInfo(typeof(ChecksumBypass.Main), ModBuildInfo.Name, ModBuildInfo.Version, ModBuildInfo.Author, ModBuildInfo.DownloadLink)]
+[assembly: MelonColor(System.ConsoleColor.Magenta)]
+// Removed so it can also be loaded in the chinese version
+// [assembly: MelonGame(ModBuildInfo.GameDeveloper, ModBuildInfo.Game)]
