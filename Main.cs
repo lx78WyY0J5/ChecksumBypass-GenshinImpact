0ae008c8309a0304b1f71184f88b1edc4744afd7
Taiga <67109235+Taiga74164@users.noreply.github.com>
2022-02-28 09:42:01 -0700

    a

--- Main.cs
+++ Main.cs
@@ -42,7 +42,7 @@ namespace ChecksumBypass {
                             MelonLogger.Msg("Setting checksum in function " + originalMethod.Name);
                             // var checksum2.2 = "601f9d24a9f983df28cdbfe40d6ca4d0c30f8297a7b37ed3b7a082df15aff9771d";
                             // var checksum2.3 = "37dfc56fd2bf5fa0d3ec55b4f2a81d374c9c8c5382645c32f97495469dbf036e1f";
-                            // var checksum = "c1218c188819cba2e794c29c6f5c0f2248eefb4cd29f29b18619e7626f83044a20";
+                            // var checksum2.4 = "c1218c188819cba2e794c29c6f5c0f2248eefb4cd29f29b18619e7626f83044a20";
                             var checksum = "6c0b1b9b176af5f26038400b229381ec952eff387d738841a7940725c457b0ae23";
                             originalDelegate(instance, IL2CPP.ManagedStringToIl2Cpp(checksum), nativeMethodPointer);
                         } else {
