4a1147571c50e3ae0d1c8f4e1a097685dd8e137b
Taiga <67109235+Taiga74164@users.noreply.github.com>
2022-04-04 17:53:07 -0600

    💀

--- Main.cs
+++ Main.cs
@@ -43,8 +43,8 @@ namespace ChecksumBypass {
                             // var checksum2.2 = "601f9d24a9f983df28cdbfe40d6ca4d0c30f8297a7b37ed3b7a082df15aff9771d";
                             // var checksum2.3 = "37dfc56fd2bf5fa0d3ec55b4f2a81d374c9c8c5382645c32f97495469dbf036e1f";
                             // var checksum2.4 = "c1218c188819cba2e794c29c6f5c0f2248eefb4cd29f29b18619e7626f83044a20";
-                            //var checksum2.5 = "6c0b1b9b176af5f26038400b229381ec952eff387d738841a7940725c457b0ae23";
-                            var checksum = "add7923a4c3fcf73996e7ad12f85a2a7952eff387d738841a7940725c457b0ae23";
+                            // var checksum2.5 = "6c0b1b9b176af5f26038400b229381ec952eff387d738841a7940725c457b0ae23";
+                            var checksum = "30a4bfb5141a1b779e00b3f69da2ab88952eff387d738841a7940725c457b0ae23";
                             originalDelegate(instance, IL2CPP.ManagedStringToIl2Cpp(checksum), nativeMethodPointer);
                         } else {
                             originalDelegate(instance, value, nativeMethodPointer);
