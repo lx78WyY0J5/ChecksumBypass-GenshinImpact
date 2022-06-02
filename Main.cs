ae4cda40cabce2d5d290c1295034eb23bcfc7bf0
Taiga <67109235+Taiga74164@users.noreply.github.com>
2022-06-02 11:51:53 -0600

    2.7 checksum

--- Main.cs
+++ Main.cs
@@ -28,7 +28,7 @@ namespace ChecksumBypass {
         public override void OnApplicationStart() {
             MelonLogger.Msg("Mod loaded.");
             unsafe {
-                var properties = typeof(Object1PublicSealedIEquatable1ObfEA1ObAHInCJStPBAIInUnique).GetProperties(BindingFlags.Instance | BindingFlags.Public);
+                var properties = typeof(Object1PublicSealedIEquatable1ObfEA1ObAHInCJStPBAIInUnique).GetProperties(BindingFlags.Instance | BindingFlags.Public); //update this yourself
 
                 foreach (var property in properties.Where(p => (p.PropertyType == typeof(string)) && (p.SetMethod.CustomAttributes.Count() == 1))) {
                     var originalMethod = property.SetMethod;
@@ -44,7 +44,8 @@ namespace ChecksumBypass {
                             // var checksum2.3 = "37dfc56fd2bf5fa0d3ec55b4f2a81d374c9c8c5382645c32f97495469dbf036e1f";
                             // var checksum2.4 = "c1218c188819cba2e794c29c6f5c0f2248eefb4cd29f29b18619e7626f83044a20";
                             // var checksum2.5 = "6c0b1b9b176af5f26038400b229381ec952eff387d738841a7940725c457b0ae23";
-                            var checksum = "30a4bfb5141a1b779e00b3f69da2ab88952eff387d738841a7940725c457b0ae23";
+                            // var checksum2.6 = "30a4bfb5141a1b779e00b3f69da2ab88952eff387d738841a7940725c457b0ae23";
+                            var checksum = "14648af3df1f47a7858d3335cf7288b044fed1eba61f15e5acaccda18967a91b23";
                             originalDelegate(instance, IL2CPP.ManagedStringToIl2Cpp(checksum), nativeMethodPointer);
                         } else {
                             originalDelegate(instance, value, nativeMethodPointer);
